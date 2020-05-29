673ca3f2cf1b8e19a968b463a0fb4ce7edef6e0a
BootWP <burdianov@gmail.com>
2020-05-29 06:46:58 +0300

    Enqueue child theme style, get theme mods from parent theme

--- functions.php
+++ functions.php
@@ -1,6 +1,25 @@
 <?php
+/**
+ * The Kandinsky Child Theme Functions
+ *
+ * @package Kandinsky
+ */
 
-if ( ! defined( 'WPINC' ) )
-	die();
+/**
+ * Wp Enqueue Styles
+ */
+function knd_child_enqueue_styles() {
+	wp_enqueue_style('knd-child', get_stylesheet_directory_uri() .'/style.css' );
+}
+add_action('wp_enqueue_scripts', 'knd_child_enqueue_styles', 99 );
 
-define('KND_CHILD_THEME', true);
+/**
+ * Copy All Parent Theme Options
+ */
+function knd_child_after_switch_theme() {
+	$parent_theme_options = get_option( 'theme_mods_kandinsky' );
+	if ( $parent_theme_options ) {
+		update_option( 'theme_mods_kandinsky-child-theme', $parent_theme_options );
+	}
+}
+add_action('after_switch_theme', 'knd_child_after_switch_theme');
