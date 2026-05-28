9d99cd0b16d392aa192af8d18ce8f923b18bb10c
BootWP <burdianov@gmail.com>
2026-05-28 14:12:56 +0300

    Improve update options on activate child theme

--- functions.php
+++ functions.php
@@ -18,12 +18,15 @@ add_action('wp_enqueue_scripts', 'knd_child_enqueue_styles' );
  * Copy All Parent Theme Options
  */
 function knd_child_after_switch_theme() {
-	$prefix  = 'theme_mods_';
-	$parent  = $prefix . get_template();
-	$child   = $prefix . get_stylesheet();
-	$options = get_option( $parent );
-	if ( $parent ) {
-		update_option( $child, $options );
+	$prefix     = 'theme_mods_';
+	$parent     = $prefix . get_template();
+	$child      = $prefix . get_stylesheet();
+	$child_opts = get_option( $child );
+	if ( empty( $child_opts ) ) {
+		$parent_opts = get_option( $parent );
+		if ( ! empty( $parent_opts ) ) {
+			update_option( $child, $parent_opts );
+		}
 	}
 }
 add_action('after_switch_theme', 'knd_child_after_switch_theme');
